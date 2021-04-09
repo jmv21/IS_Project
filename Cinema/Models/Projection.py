056df69a79fed2cf3a85fe10c7591e865e4db825
jmv21 <78463691+jmv21@users.noreply.github.com>
2021-04-09 00:34:12 -0400

    Revert "Revert "cambios""
    This reverts commit 540c4c63eb189102a47de911cfcdbf2dd1c33468.

--- Cinema/Models/Projection.py
+++ Cinema/Models/Projection.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.core.exceptions import ValidationError
 from .Hall import Hall
@@ -8,7 +9,12 @@ from .Time import Time
 class Projection(models.Model):
     hall = models.ForeignKey(Hall, blank=False, null=True, on_delete=models.SET_NULL)
     movie = models.ForeignKey(Movie, blank=False, null=False, on_delete=models.CASCADE)
-    time = models.ForeignKey(Time, blank=False, null=True, on_delete=models.SET_NULL)
+    time = models.ForeignKey(Time, blank=False, null=True, on_delete=models.CASCADE)
+    monetary_cost = models.DecimalField(name="monetary cost", default=20, decimal_places=2, max_digits=8,
+                                        validators=[MinValueValidator(0)])
+    partner_points_cost = models.DecimalField(name='partner points cost', default=20, decimal_places=2, max_digits=8,
+                                              validators=[MinValueValidator(0)])
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.movie.name} in {self.hall} at {self.time.beginning_time}"
@@ -19,5 +25,3 @@ class Projection(models.Model):
 
     class Meta:
         unique_together = [["hall", "movie"], ["hall", "time"], ["movie", "time"]]
-
-
