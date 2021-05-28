fb804631df46681ae833056a3307cf97dcaf39f6
jmv21 <78463691+jmv21@users.noreply.github.com>
2021-05-28 13:30:07 -0400

    algo

--- IS_Project/settings.py
+++ IS_Project/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_REDIRECT_URL= '/'
+
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
 
@@ -125,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
