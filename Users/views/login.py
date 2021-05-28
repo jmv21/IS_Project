2df75bc37fe36ea1b5a01665bfe98927dc1c8477
jmv21 <78463691+jmv21@users.noreply.github.com>
2021-05-28 13:17:31 -0400

    requerimientos actualizados

--- Users/views/login.py
+++ Users/views/login.py
@@ -1,7 +1,5 @@
 from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView, LogoutView
-from  django.utils.decorators import method_decorator
-from d
 from django.shortcuts import render, redirect
 from django.views.generic import RedirectView
 
@@ -12,7 +10,6 @@ from django.utils.decorators import method_decorator
 class LoginFormView(LoginView):
     template_name = 'login.html'
 
-    @method_decorator(sensitive_post_parameters())
     def dispatch(self, request, *args, **kwargs):
         if(request.user.is_authenticated):
             return redirect('/')
